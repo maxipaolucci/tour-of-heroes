54461269757f420ee3bc400e78fff37cc7facb2a
Maximiliano Paolucci <maximiliano.paolucci@vodafone.com>
2016-08-05 15:56:33 +1200

    added service and promise

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,22 +1,25 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+
 
 
 @Component({
   selector: 'my-app',
-  template:`
-  <h1>{{title}}</h1>
-  <h2>My Heroes</h2>
-  <ul class="heroes">
-    <li *ngFor="let hero of heroes" 
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes"
         [class.selected]="hero === selectedHero"
         (click)="onSelect(hero)">
-      <span class="badge">{{hero.id}}</span> {{hero.name}}
-    </li>
-  </ul>
-  
-  <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
+  directives: [ HeroDetailComponent ],
   styles: [`
     .selected {
       background-color: #CFD8DC !important;
@@ -66,28 +69,24 @@ import { HeroDetailComponent } from './hero-detail.component';
       border-radius: 4px 0 0 4px;
     }
   `],
-  directives: [HeroDetailComponent]
+  providers: [HeroService]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  selectedHero : Hero;
+  heroes: Hero[];
+  selectedHero: Hero;
 
-  public heroes = HEROES;
+  constructor(private heroService: HeroService) { }
 
-  onSelect(hero: Hero) {
-    this.selectedHero = hero;
+  ngOnInit() {
+    this.getHeroes();
   }
-}
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+  onSelect(hero: Hero) { this.selectedHero = hero; }
+
+  getHeroes() {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+
+}
