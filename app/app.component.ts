7ac2351b97ecd699189f0bc058df81f890b3eb4f
Maxi Paolucci <maxipaolucci@gmail.com>
2016-08-06 14:12:08 +1200

    added dashboard more routing and more services and components

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,9 +6,13 @@ import {HeroService} from "./hero.service";
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <a [routerLink]="['/heroes']">Heroes</a>
+    <nav>
+      <a [routerLink]="['/dashboard']" routerLinkActive="active">Dashboard</a>
+      <a [routerLink]="['/heroes']" routerLinkActive="active">Heroes</a>
+    </nav>
     <router-outlet></router-outlet>
   `,
+  styleUrls: ['app/app.component.css'],
   directives: [ROUTER_DIRECTIVES],
   providers: [HeroService]
 })
--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,7 +1,22 @@
 import { provideRouter, RouterConfig }  from '@angular/router';
 import { HeroesComponent } from './heroes.component';
+import {DashboardComponent} from "./dashboard.component";
+import { HeroDetailComponent } from './hero-detail.component';
 
 const routes: RouterConfig = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'detail/:id',
+    component: HeroDetailComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
   {
     path: 'heroes',
     component: HeroesComponent
