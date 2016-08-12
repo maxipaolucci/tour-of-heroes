2a5ff5b6f60a7ccdc06c604206df9b8237432a34
Maximiliano Paolucci <maximiliano.paolucci@vodafone.com>
2016-08-12 16:09:49 +1200

    added http services

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,31 +1,34 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
-
+// Imports for loading & configuring the in-memory web api
+import { HttpModule, XHRBackend } from '@angular/http';
+import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }               from './in-memory-data.service';
 import { AppComponent }   from './app.component';
-import { HeroesComponent }  from './heroes.component';
-import { HeroDetailComponent } from "./hero-detail.component";
-import {DashboardComponent} from "./dashboard.component";
-
-import { HeroService }  from './hero.service';
-
 import { routing }        from './app.routing';
-
+import { HeroesComponent }      from './heroes.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroService }  from './hero.service';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    routing
+    routing,
+    HttpModule
   ],
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroDetailComponent
   ],
   providers: [
-    HeroService
+    HeroService,
+    { provide: XHRBackend, useClass: InMemoryBackendService }, // in-mem server
+    { provide: SEED_DATA,  useClass: InMemoryDataService }     // in-mem server data
   ],
   bootstrap: [ AppComponent ]
 })
